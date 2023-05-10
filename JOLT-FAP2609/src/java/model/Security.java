d4c6c2ea8abb6adbcef7a0a6355207eb991ad7fc
jay <86312510+mojay2@users.noreply.github.com>
2023-05-11 03:44:38 +0800

    Add model for encrypt and decrypt

---
+++ JOLT-FAP2609/src/java/model/Security.java
@@ -0,0 +1,38 @@
+
+package model;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.*;
+
+public class Security {
+
+public static String encrypt(String strToEncrypt, String CipherType, String keyString) {
+                byte[] key = keyString.getBytes();
+		String encryptedString = null;
+		try {
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			encryptedString = Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes()));
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return encryptedString;
+	}
+
+	public static String decrypt(String codeDecrypt, String CipherType, String keyString){
+                byte[] key = keyString.getBytes();
+		String decryptedString = null;
+		try{
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+			final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+			decryptedString = new String(cipher.doFinal(Base64.decodeBase64(codeDecrypt)));
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return decryptedString;
+    }
+}
+
